c45a1b4e991eb182748dcfd64da2db45a5e1bdde
Mega <83192893+MegaCoder2@users.noreply.github.com>
2021-12-22 18:45:51 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function start()
     navigator.mediaDevices.getUserMedia({
         audio: true
     })
-    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/uvA7CIyct/model.json", modelready)
+    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/vvQRp3Def/model.json", modelready)
 }
 function modelready()
 {
@@ -12,5 +12,32 @@ function modelready()
 }
 function gotresult(error, result)
 {
-    console.log(result)
+    if(error)
+    {
+        console.error(error)
+    }
+    else
+    {
+        console.log(result)
+        r = Math.floor(Math.random()*255) + 1
+        g = Math.floor(Math.random()*255) + 1
+        b = Math.floor(Math.random()*255) + 1
+        document.getElementById("result").style.color = "rgb("+r+", "+g+", "+b+")"
+        document.getElementById("confindent").style.color = "rgb("+r+", "+g+", "+b+")"
+        document.getElementById("result").innerHTML = "I can hear " + result[0].label + "!"
+        document.getElementById("confindent").innerHTML = "Accuracy: " + (result[0].confidence * 100).toFixed(2) + "%"
+        img = document.getElementById("image")
+        if(result[0].label == "Cat Meowing")
+        {
+            img.src = "meow-e1612557327342.jpeg"
+        }
+        else if(result[0].label == "Dog Barking")
+        {
+            img.src = "Yellow-Lab-Puppy-Barking-500x500.jpg"
+        }
+        else
+        {
+            img.src = "listen.gif"
+        }
+    }
 }
